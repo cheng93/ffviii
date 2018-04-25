93cc1ddb877649eb21358793145f51a1e10a31c9
cheng93 <derek.c@hotmail.co.uk>
2018-04-25 22:32:16 +0100

    guardian-force component

---
+++ app/components/abilities/abilities.component.ts
@@ -0,0 +1,11 @@
+import { OnInit, Component } from '@angular/core';
+
+@Component({
+    selector: 'abilities',
+    template: require('./abilities.template.html')
+})
+export class AbilitiesComponent implements OnInit {
+    ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
+}
--- app/components/guardian-force/guardian-force.component.ts
+++ app/components/guardian-force/guardian-force.component.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GuardianForce } from '../../models/guardian-forces';
 import { GuardianForceService } from '../../services/guardian-force.service';
+import { CharacterService } from '../../services/character.service';
+import { Character } from '../../models/character';
 
 @Component({
     selector: 'guardian-force',
     template: require('./guardian-force.template.html')
 })
-export class GuardianForceComponent {
-    constructor(private guardianForceService: GuardianForceService) {
-        guardianForceService.getAll().subscribe(x => (this.guardianForces = x));
+export class GuardianForceComponent implements OnInit {
+    constructor(
+        private guardianForceService: GuardianForceService,
+        private characterService: CharacterService
+    ) {}
+    ngOnInit(): void {
+        this.guardianForceService
+            .getAll()
+            .subscribe(x => (this.guardianForces = x));
+
+        this.characterService.getAll().subscribe(x => (this.characters = x));
     }
+
+    characters: Character[] = [];
 
     guardianForces: GuardianForce[] = [];
 }
