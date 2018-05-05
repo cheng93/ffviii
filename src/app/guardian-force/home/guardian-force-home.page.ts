6341ecfd2d837735cb495b37a332ed4aaad4ec86
cheng93 <derek.c@hotmail.co.uk>
2018-05-05 16:43:41 +0100

    moved all of styles into components

--- src/app/guardian-force/home/guardian-force-home.page.ts
+++ src/app/guardian-force/home/guardian-force-home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    templateUrl: './guardian-force-home.template.html'
+    templateUrl: './guardian-force-home.template.html',
+    styleUrls: ['./guardian-force-home.style.scss']
 })
 export class GuardianForceHomePage {}
